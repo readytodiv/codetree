aa84b433a3d25c74acaf33d249234bab7d37d3bd
GOLD.XY <109033026+readytodiv@users.noreply.github.com>
2026-03-14 12:50:58 +0900

    Title: 굉장한 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260314/굉장한 수/amazing-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    if((N%2==1 && N%3==0) || (N%2==0 && N%5==0))
+    {
+        cout << "true";
+
+    }
+    else
+    {
+        cout << "false";
+    }
+    return 0;
+}
