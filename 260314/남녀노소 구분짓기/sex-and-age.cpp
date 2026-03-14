c9aa2408234ea7a8ca55fe05a74630a34284a6f2
GOLD.XY <109033026+readytodiv@users.noreply.github.com>
2026-03-14 12:56:19 +0900

    Title: 남녀노소 구분짓기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260314/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sex;
+    cin >> sex;
+
+    int age;
+    cin >> age;
+
+    if(sex ==0)
+    {
+        if(age>=19){
+            cout << "MAN";
+        }
+        else
+        {
+            cout <<"BOY";
+        }
+    }
+    else
+    {
+        if(age >=19)
+        {
+            cout << "WOMAN";
+        }
+        else
+        {
+            cout << "GIRL";
+        }
+    }
+    return 0;
+}
