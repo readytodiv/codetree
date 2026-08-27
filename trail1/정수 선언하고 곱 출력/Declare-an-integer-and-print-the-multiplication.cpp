9ab80fddf0bc2e8944ddb35e2b01570b0ddbc8cf
GOLD.XY <109033026+readytodiv@users.noreply.github.com>
2026-08-28 00:25:23 +0900

    [Passed] 정수 선언하고 곱 출력 (2ms, 0MB)

---
+++ trail1/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 26;
+    int b = 5;
+
+    cout << "26 * 5 = "<< 130;
+    return 0;
+}
