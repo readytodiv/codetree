fae5b4ac4c9bda382311af643c59453025971f30
GOLD.XY <109033026+readytodiv@users.noreply.github.com>
2026-08-29 21:03:09 +0900

    [Passed] 합을 복사하기 (2ms, 0MB)

---
+++ trail1/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =1;
+    int b = 2;
+    int c = 3;
+    int sum = a+b+c;
+
+    a = sum;
+    b = sum;
+    c = sum;
+    
+    cout <<a <<" "<<b <<" "<<c <<" ";
+    return 0;
+}
