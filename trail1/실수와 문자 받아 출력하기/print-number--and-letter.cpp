dcf9225383a25f074463bddab1b76e9ac45877d4
GOLD.XY <109033026+readytodiv@users.noreply.github.com>
2026-09-01 00:01:25 +0900

    [Passed] 실수와 문자 받아 출력하기 (2ms, 0MB)

---
+++ trail1/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    double a, b;
+    cout << fixed;
+    cout.precision(2);
+    cin >> c >> a >> b;
+    cout << c <<"\n"<< a <<"\n"<< b;
+    return 0;
+}
