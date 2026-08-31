c535f6a788d2ffc4eecccdda3e18241cf89c2875
GOLD.XY <109033026+readytodiv@users.noreply.github.com>
2026-09-01 01:15:04 +0900

    [Passed] 날짜 변경하여 출력 (1ms, 0MB)

---
+++ trail1/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+    int month;
+    int day;
+    char dot;
+    char dash ='-';
+
+    cin >> year >> dot >> month >>dot >>day;
+    cout << month <<dash <<day <<dash<< year;
+
+    return 0;
+}
