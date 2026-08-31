2cc0ff54dc03fa06364f776cc464570b08d0fc36
GOLD.XY <109033026+readytodiv@users.noreply.github.com>
2026-09-01 01:02:11 +0900

    [Passed] 날짜 변경하여 출력 2 (2ms, 0MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mm, dd, yyyy;
+    char c;
+    char d ='.';
+    cin >>mm >>c>>dd>>c>>yyyy;
+    cout << yyyy << d << mm << d << dd;
+    return 0;
+}
