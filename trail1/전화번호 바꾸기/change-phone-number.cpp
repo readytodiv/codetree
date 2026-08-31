5a81e6a05cf620193d928d31d4b7b9632a5b9503
GOLD.XY <109033026+readytodiv@users.noreply.github.com>
2026-09-01 01:36:00 +0900

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x, y;
+    char c ='-';
+    cin.get();
+    cin.get();
+    cin.get();
+    cin.get();
+    cin >> x >> c>> y;
+    cout << "010" << c << y << c<<x;
+    return 0;
+}
